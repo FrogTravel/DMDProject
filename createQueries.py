868073fcb8258b85680996fd482df004086a1d36
FrogTravel <e.levchenko@innopolis.ru>
2017-11-23 00:31:27 +0300

    Test version of db

--- createQueries.py
+++ createQueries.py
@@ -8,16 +8,24 @@ import DBStructure
 # TODO solve ID that is foreign key. Also order is the weak entity
 # TODO accept is weak many-to-one relationship with car
 # TODO supervice strong rel, many-to-one with manager
+# TODO Not sure that manager id is not null...
 CREATE_ORDER = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.ORDER_TABLE_NAME + ' (' \
-               + DBStructure.ORDER_TABLE_FIELD_ID + ' integer AUTOINCREMENT,' \
+               + DBStructure.ORDER_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
                + DBStructure.ORDER_TABLE_FIELD_STARTING_POSITION + ' varchar(255) NOT NULL,' \
-               + DBStructure.ORDER_TABLE_FIELD_DESTINATION + ' varchar(255) NOT NULL' \
-               + DBStructure.ORDER_TABLE_FIELD_STATE + ' varchar(255) NOT NULL' \
-               + DBStructure.ORDER_TABLE_FIELD_START_TIME + ' varchar(255) NOT NULL,' \
-               + DBStructure.ORDER_TABLE_FIELD_END_TIME + ' varchar(255) NOT NULL,' \
+               + DBStructure.ORDER_TABLE_FIELD_DESTINATION + ' varchar(255) NOT NULL, ' \
+               + DBStructure.ORDER_TABLE_FIELD_START_TIME + ' datetime NOT NULL,' \
+               + DBStructure.ORDER_TABLE_FIELD_END_TIME + ' datetime NOT NULL,' \
                + DBStructure.ORDER_TABLE_FIELD_PRICE + ' integer NOT NULL, ' \
-               + DBStructure.ORDER_TABLE_FIELD_SATISFACTION + ' integer' \
-                                                              ');'
+               + DBStructure.ORDER_TABLE_FIELD_SATISFACTION + ' integer, ' \
+               + DBStructure.ORDER_TABLE_FIELD_MANAGER_ID + ' integer NOT NULL REFERENCES ' \
+               + DBStructure.MANAGER_TABLE_NAME + ', ' \
+               + DBStructure.ORDER_TABLE_FIELD_CLIENT_ID + ' integer NOT NULL REFERENCES ' \
+               + DBStructure.CLIENT_TABLE_NAME + ', ' \
+               + DBStructure.ORDER_TABLE_FIELD_CAR_ID + ' integer NOT NULL REFERENCES ' \
+               + DBStructure.CAR_TABLE_NAME + ', ' \
+               + DBStructure.ORDER_TABLE_FIELD_STATE_ID + ' integer NOT NULL REFERENCES ' \
+               + DBStructure.STATE_TABLE_NAME + '' \
+                                                ');'
 
 # TODO 'lives at' is 0...N to 1..N relationship
 # AM... No Primary key at this table, so let it be some ID
@@ -46,8 +54,12 @@ CREATE_CAR = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.CAR_TABLE_NAME + ' (' \
              + DBStructure.CAR_TABLE_FIELD_NUMBER + ' varchar(10) NOT NULL, ' \
              + DBStructure.CAR_TABLE_FIELD_MODEL + ' varchar(10) NOT NULL,' \
              + DBStructure.CAR_TABLE_FIELD_CHARGE_LEVEL + ' varchar(10) NOT NULL,' \
-             + DBStructure.CAR_TABLE_FIELD_LOCATION + ' varchar(10) NOT NULL' \
-                                                      ');'
+             + DBStructure.CAR_TABLE_FIELD_LOCATION + ' varchar(10) NOT NULL,' \
+             + DBStructure.CAR_TABLE_LAT + ' real NOT NULL,' \
+             + DBStructure.CAR_TABLE_LNG + ' real NOT NULL,' \
+             + DBStructure.CAR_TABLE_ALL_CARS + ' integer NOT NULL REFERENCES ' \
+             + DBStructure.ALL_CARS_TABLE_NAME + '' \
+                                                 ');'
 
 # same problem with start_time
 # what is the type of content
@@ -62,24 +74,85 @@ CREATE_VIDEO = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.VIDEO_TABLE_NAME + ' 
 
 # TODO 'accept' is weak relationship many-to-one with Charger
 # TODO weak entity
-CREATE_CHARGE_ORDER = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.CHARGE_ORDER_TABLE_NAME + ' (' \
-                      + DBStructure.CHARGE_ORDER_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
-                      + DBStructure.CHARGE_ORDER_TABLE_FIELD_CHARGE_TIME + ' varchar(255) NOT NULL, ' \
-                      + DBStructure.CHARGE_ORDER_TABLE_FIELD_ARRIVAL_TIME + ' DATA NOT NULL,' \
-                      + DBStructure.CHARGE_ORDER_TABLE_FIELD_AMOUNT_OF_TAKEN_ENERGY + ' integer NOT NULL' \
-                                                                                      ');'
+CREATE_REFILL_STATION = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.REFILL_STATION_TABLE_NAME + ' (' \
+                        + DBStructure.REFILL_STATION_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
+                        + DBStructure.REFILL_STATION_TABLE_FIELD_CHARGE_TIME + ' varchar(255) NOT NULL, ' \
+                        + DBStructure.REFILL_STATION_TABLE_FIELD_ARRIVAL_TIME + ' DATETIME NOT NULL,' \
+                        + DBStructure.REFILL_STATION_TABLE_FIELD_AMOUNT_OF_TAKEN_ENERGY + ' integer NOT NULL ' \
+                                                                                          ');'
 
 # TODO weak entity
 # Again, what is the type of location, may be we should change it to lat lang
-CREATE_CHARGER = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.CHARGER_TABLE_NAME + ' (' \
-                 + DBStructure.CHARGER_TABLE_FIELD_LOCATION + ' varchar(255) NOT NULL,' \
-                 + DBStructure.CHARGER_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
-                 + DBStructure.CHARGER_TABLE_FIELD_TOTAL_AMOUNT_OF_CHARGERS + ' integer NOT NULL,' \
-                 + DBStructure.CHARGER_TABLE_FIELD_AMOUNT_OF_FREE_CHARGERS + ' integer NOT NULL' \
-                                                                             ');'
+CREATE_CHARGE_EVENT = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.CHARGE_EVENT_TABLE_NAME + ' (' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_LOCATION + ' varchar(255) NOT NULL,' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_TOTAL_AMOUNT_OF_CHARGERS + ' integer NOT NULL,' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_AMOUNT_OF_FREE_CHARGERS + ' integer NOT NULL,' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_COST + ' integer NOT NULL, ' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_DATE + ' datetime NOT NULL, ' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_CAR_ID + ' integer NOT NULL REFERENCES ' \
+                      + DBStructure.ALL_CARS_TABLE_NAME + ',' \
+                      + DBStructure.CHARGE_EVENT_TABLE_FIELD_REFILL_STATION_ID + ' integer NOT NULL REFERENCES ' \
+                      + DBStructure.REFILL_STATION_TABLE_NAME + '' \
+                                                                ');'
 
 # TODO Relationship 'manage(ban/restrict)' is 1..N to 0..N
 CREATE_MANAGER = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.MANAGER_TABLE_NAME + ' (' \
-                 + DBStructure.MANAGER_TABLE_FIELD_ID + 'integer PRIMARY KEY AUTOINCREMENT,' \
-                 + DBStructure.MANAGER_TABLE_FIELD_NAME + ' varchar(255) NOT NULL' \
-                                                          ');'
+                 + DBStructure.MANAGER_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT,' \
+                 + DBStructure.MANAGER_TABLE_FIELD_NAME + ' varchar(255) NOT NULL, ' \
+                 + DBStructure.MANAGER_TABLE_PHONE + ' varchar(255) NOT NULL ' \
+                                                     ');'
+
+# Many to many Client Location 0..N 1..N relation
+# TODO what here is primary key
+# TODO how to make 0..N _1..N_
+CREATE_RELATION_CLIENT_LOCATION = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.RELATION_CLIENT_LOCATION_NAME + ' (' \
+                                  + DBStructure.RELATION_CLIENT_LOCATION_FIELD_LOCATION_ID + ' integer NOT NULL ,' \
+                                  + DBStructure.RELATION_CLIENT_LOCATION_FIELD_CLIENT_USERNAME + ' varchar(255) NOT NULL' \
+                                                                                                 ');'
+
+# Many to many Manager Client 1..N 0..N
+# TODO _1..N_ 0..N
+CREATE_RELATION_MANAGER_CLIENT = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.RELATION_MANAGER_CLIENT_NAME + ' (' \
+                                 + DBStructure.RELATION_MANAGER_CLIENT_FIELD_CLIENT_USERNAME + ' varchar(255) NOT NULL ,' \
+                                 + DBStructure.RELATION_MANAGER_CLIENT_FIELD_MANAGER_ID + ' integer NOT NULL' \
+                                                                                          ');'
+
+CREATE_LIVES_AT_RELATION_TABLE = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.RELATION_TABLE_LIVES_AT_NAME + ' (' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_LOCATION_ID + ' integer NOT NULL, ' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_CLIENT_ID + ' integer NOT NULL,' \
+                                                                                   'PRIMARY KEY (' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_CLIENT_ID + ', ' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_LOCATION_ID + '),' \
+                                                                                     'FOREIGN KEY (' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_CLIENT_ID + ') REFERENCES ' \
+                                 + DBStructure.CLIENT_TABLE_NAME + ', ' \
+                                                                   'FOREIGN KEY (' \
+                                 + DBStructure.RELATION_TABLE_LIVES_AT_LOCATION_ID + ') REFERENCES ' \
+                                 + DBStructure.LOCATION_TABLE_NAME + '' \
+                                                                     '); '
+
+CREATE_STATE = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.STATE_TABLE_NAME + ' (' \
+               + DBStructure.STATE_TABLE_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT, ' \
+               + DBStructure.STATE_TABLE_FIELD_STATE_NAME + ' varchar(255) NOT NULL' \
+                                                            ');'
+
+CREATE_ALL_CARS = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.ALL_CARS_TABLE_NAME + ' (' \
+                  + DBStructure.ALL_CARS_FIELD_ID + ' integer PRIMARY KEY AUTOINCREMENT, ' \
+                  + DBStructure.ALL_CARS_FIELD_COLOR + ' varchar(255) NOT NULL, ' \
+                  + DBStructure.ALL_CARD_FIELD_MANUFACTURE + ' varchar(255) NOT NULL, ' \
+                  + DBStructure.ALL_CARS_FIELD_NUMBER + ' varchar(255) NOT NULL ' \
+                                                        ');'
+
+CREATE_PAYMENT = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.PAYMENT_TABLE_NAME + ' (' \
+                 + DBStructure.PAYMENT_TABLE_FIELD_ID + ' integer PRIMARY KEY,' \
+                 + DBStructure.PAYMENT_TABLE_FIELD_ORDER_ID + ' integer NOT NULL REFERENCES ' \
+                 + DBStructure.ORDER_TABLE_NAME + ',' \
+                 + DBStructure.PAYMENT_TABLE_FIELD_HASH_CREDIT_CARD + ' varchar(255) NOT NULL' \
+                                                                      ');'
+# Order - weak entity
+# Order manager many to one
+# CREATE_RELATION_ORDER_MANAGER = 'CREATE TABLE IF NOT EXISTS ' + DBStructure.RELATION_ORDER_MANAGER_NAME + ' (' \
+#                                   + DBStructure.RELATION_ORDER_MANAGER_FIELD_MANAGER_ID + ' integer NOT NULL ,' \
+#                                   + DBStructure.RELATION_ORDER_MANAGER_FIELD_ORDER_ID + ' integer NOT NULL' \
+#                                                                                                  ');'
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import sqlite3
+import createQueries
 #http://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
 
-print(sqlite3.sqlite_version)
 
 conn = sqlite3.connect(r"/Users/ekaterina/PycharmProjects/DMDProject3/myfirstdb.db")
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS TestTable ("
-               "id integer PRIMARY KEY AUTOINCREMENT,"
-               "first_name text NOT NULL,"
-               "last_name text NOT NULL);")
-cursor.execute("INSERT INTO TestTable (first_name, last_name) VALUES (\"Mary\", \"Johns\"), (\"Bred\", \"Stevens\");")
-cursor.execute("SELECT * FROM TestTable;")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+# cursor.execute("CREATE TABLE IF NOT EXISTS TestTable ("
+#                "id integer PRIMARY KEY AUTOINCREMENT,"
+#                "first_name text NOT NULL,"
+#                "last_name text NOT NULL);")
+# cursor.execute("INSERT INTO TestTable (first_name, last_name) VALUES (\"Mary\", \"Johns\"), (\"Bred\", \"Stevens\");")
+cursor.execute(createQueries.CREATE_STATE)
+cursor.execute(createQueries.CREATE_LOCATION)
+cursor.execute(createQueries.CREATE_CLIENT)
+cursor.execute(createQueries.CREATE_LIVES_AT_RELATION_TABLE)
+cursor.execute(createQueries.CREATE_MANAGER)
+cursor.execute(createQueries.CREATE_ALL_CARS)
+cursor.execute(createQueries.CREATE_REFILL_STATION)
+cursor.execute(createQueries.CREATE_CAR)
+cursor.execute(createQueries.CREATE_REFILL_STATION)
+cursor.execute(createQueries.CREATE_CHARGE_EVENT)
+print(createQueries.CREATE_ORDER)
+cursor.execute(createQueries.CREATE_ORDER)
+cursor.execute(createQueries.CREATE_PAYMENT)
+
+
+
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row)
+
+print(createQueries.CREATE_LIVES_AT_RELATION_TABLE)
